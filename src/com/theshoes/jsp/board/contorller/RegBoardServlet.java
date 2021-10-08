ceb97f50af2bc038c69b19c63a27ca42cb9fe5ee
yujeo <yujeo@CAD_TOP>
2021-10-09 00:20:19 +0900

    20211009 00:17 유정
    1. BoardDTO 변수 주석 생성
    2. mybatis-config.xml
    3. RegBoardServlet 약간 수정

--- src/com/theshoes/jsp/board/contorller/RegBoardServlet.java
+++ src/com/theshoes/jsp/board/contorller/RegBoardServlet.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 public class RegBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/* 공지사항 작성 후 get 요청을 할 경우 처리하는 역할 */
+	/* '등록하기' 버튼 클릭 시 get 요청 ->  */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String noticeTitle = request.getParameter("noticeTitle");
 		String noticeWriter = request.getParameter("noticeWriter");
 		String noticeDetail = request.getParameter("noticeDetail");
 	}
 
+	/* 공지사항 작성 후 post 요청을 할 경우 처리하는 역할 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
