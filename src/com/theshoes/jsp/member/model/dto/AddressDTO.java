a37a3bfeb3b81423023ca383a5bf8a32d5d8c382
Jinn <quizh@DESKTOP-HTFFCH1>
2021-10-12 00:07:49 +0900

    211012 00:07 호진 마스터 커밋

--- src/com/theshoes/jsp/member/model/dto/AddressDTO.java
+++ src/com/theshoes/jsp/member/model/dto/AddressDTO.java
@@ -2,68 +2,62 @@ package com.theshoes.jsp.member.model.dto;
 
 public class AddressDTO implements java.io.Serializable{
 	
+	private String nameMM; 
+	private String addressNo;
 	private String addressName;
-	private String nameMM;
-	private String phoneMM; 
-	private String address1MM; 
+	private String address1MM;
 	private String address2MM;
-	
 	public AddressDTO() {
-		System.out.println("AddressDTO");
 	}
-
-	public AddressDTO(String addressName, String nameMM, String phoneMM, String address1mm, String address2mm) {
-		this.addressName = addressName;
+	public AddressDTO(String nameMM, String addressNo, String addressName, String address1mm, String address2mm) {
 		this.nameMM = nameMM;
-		this.phoneMM = phoneMM;
+		this.addressNo = addressNo;
+		this.addressName = addressName;
 		address1MM = address1mm;
 		address2MM = address2mm;
 	}
-
-	public String getAddressName() {
-		return addressName;
-	}
-
-	public void setAddressName(String addressName) {
-		this.addressName = addressName;
-	}
-
 	public String getNameMM() {
 		return nameMM;
 	}
-
 	public void setNameMM(String nameMM) {
 		this.nameMM = nameMM;
 	}
-
-	public String getPhoneMM() {
-		return phoneMM;
+	public String getAddressNo() {
+		return addressNo;
 	}
-
-	public void setPhoneMM(String phoneMM) {
-		this.phoneMM = phoneMM;
+	public void setAddressNo(String addressNo) {
+		this.addressNo = addressNo;
 	}
-
+	public String getAddressName() {
+		return addressName;
+	}
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
+	}
 	public String getAddress1MM() {
 		return address1MM;
 	}
-
 	public void setAddress1MM(String address1mm) {
 		address1MM = address1mm;
 	}
-
 	public String getAddress2MM() {
 		return address2MM;
 	}
-
 	public void setAddress2MM(String address2mm) {
 		address2MM = address2mm;
 	}
-
 	@Override
 	public String toString() {
-		return "AddressDTO [addressName=" + addressName + ", nameMM=" + nameMM + ", phoneMM=" + phoneMM
+		return "AddressDTO [nameMM=" + nameMM + ", addressNo=" + addressNo + ", addressName=" + addressName
 				+ ", address1MM=" + address1MM + ", address2MM=" + address2MM + "]";
 	}
 
+	
+	
+
+	
 }
+
+
+
+
