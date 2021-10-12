33f55a983281245dd9bee1b983293ee70cd91b7b
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2021-10-12 10:46:00 +0900

    20211012 10:45 황성연
    shoes/controller 클래스들 클래스명 수정

--- src/com/theshoes/jsp/shoes/controller/PayShoesServlet.java
+++ src/com/theshoes/jsp/shoes/controller/PayShoesServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/shoes/detail")
-public class shoesDetailServlet extends HttpServlet {
+@WebServlet("/shoes/pay")
+public class PayShoesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
