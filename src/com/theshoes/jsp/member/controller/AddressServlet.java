d63437aec206b482e54f9d3b3dabb9182434380d
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2021-10-14 21:18:40 +0900

    20211014 19:18 황성연
    -

--- src/com/theshoes/jsp/member/controller/AddressServlet.java
+++ src/com/theshoes/jsp/member/controller/AddressServlet.java
@@ -1,6 +1,7 @@
 package com.theshoes.jsp.member.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.theshoes.jsp.common.paging.SelectCriteria;
 import com.theshoes.jsp.member.model.dto.AddressDTO;
 import com.theshoes.jsp.member.model.dto.MemberDTO;
 import com.theshoes.jsp.member.model.serivce.AddressService;
@@ -19,23 +21,37 @@ public class AddressServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		String path = "/WEB-INF/views/myPage/myAddress.jsp";
+		HttpSession session = request.getSession();
+		String id =  ((MemberDTO)session.getAttribute("entryMember")).getId();
+		/* 전체 주소록 조회 */ 
+		List<AddressDTO> addressList = new AddressService().selectAllAddressList(id);
 		
+		System.out.println("addressList : " + addressList);
 		
+		SelectCriteria selectCriteria = null;
+		
+		String path = "";
+		if(addressList != null) {
+			path = "/WEB-INF/views/myPage/myAddress.jsp";
+			request.setAttribute("addressList", addressList);
+		} else {
+			path = "/WEB-INF/views/common/errorPage.jsp";
+		}
 		request.getRequestDispatcher(path).forward(request, response);
-	}	
 		
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		/* address 추가 */  
 		HttpSession session = request.getSession();
 		String nameMM =  ((MemberDTO)session.getAttribute("entryMember")).getId();
-		String addressNo = request.getParameter("addressNoMM");
+		String addressNo = request.getParameter("addressNo");
 		String addressName = request.getParameter("addressNameMM"); 
 		String address1MM = request.getParameter("address1"); 
 		String address2MM = request.getParameter("address2");
 		
-		System.out.println("AddressServlet");
+		System.out.println("AddressServlet 쓰윽~");
 		
 		AddressDTO newAddress = new AddressDTO();
 		newAddress.setNameMM(nameMM);
@@ -49,9 +65,18 @@ public class AddressServlet extends HttpServlet {
 		AddressService addressService = new AddressService();
 		int result = addressService.insertAddress(newAddress);
 		
+		String path = "";
+		
+		/* address 추가 후 주소록 메인페이지로 이동  */
+		if(result > 0) {
+			path = "/WEB-INF/views/common/success.jsp";
+			request.setAttribute("myAddressCode", "insertAddress");
+		} else {
+			path = "/WEB-INF/views/common/errorPage.jsp";
+		}
+		request.getRequestDispatcher(path).forward(request, response);
 	}
 	
-		/* address 수정 */ 
-	
+
 	
 }
--- src/com/theshoes/jsp/member/model/dao/AddressDAO.java
+++ src/com/theshoes/jsp/member/model/dao/AddressDAO.java
@@ -4,25 +4,30 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.theshoes.jsp.board.model.dto.BoardDTO;
 import com.theshoes.jsp.common.paging.SelectCriteria;
 import com.theshoes.jsp.member.model.dto.AddressDTO;
 
 public class AddressDAO {
 
-	/*주소록 전체 조회용 메소드 */ 
-	public List<AddressDTO> selectAllAddressList(SqlSession session) {
-		return null;
-	
-	}
-	
 	
 	/* 주소록 추가 메소드 */ 
 	public int insertAddress(SqlSession session, AddressDTO newAddress) {
-		System.out.println("나는야 DAO DAO DAO ");
 		return session.insert("AddressDAO.insertAddress",newAddress);
 			
-	/* 주소록 수정 메소드 */ 
-		
 	}
+
+	/* 주소록 목록 전체 조회 메소드 */
+	public static List<AddressDTO> selectAllAddressList(SqlSession session, String id) {
+		return session.selectList("AddressDAO.selectAllAddressList", id);
+	}
+
+	/* 주소록 수정 메소드 */ 
+	public int updateAddress(SqlSession session, AddressDTO address) {
+		return session.update("AddressDAO.updateAddress", address);
+
+	
+	}
+
 
 }
