70545bc4cd2c4c89ef30e2c3f6331bb25f270306
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2021-10-15 17:13:32 +0900

    20211015 17:13 황성연
    resellList 하이퍼링크 수정

--- src/com/theshoes/jsp/board/contorller/ResellListServlet.java
+++ src/com/theshoes/jsp/board/contorller/ResellListServlet.java
@@ -22,9 +22,6 @@ public class ResellListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/resell/resellList.jsp").forward(request, response);
-
-//		request.setCharacterEncoding("UTF-8");		// UTF-8 포맷 변환
 		
 		/* 페이지 하나에 보여줄 게시물 수 */
 		int limit = 12;
@@ -35,26 +32,13 @@ public class ResellListServlet extends HttpServlet {
 		/* 기본 시작 페이지 번호 */
 		int pageNo = 1;
 		
-		/* 페이지 목록 보기 */
-		String currentPage = request.getParameter("currentPage");
-		
-		if (currentPage != null && !"".equals(currentPage)) {
-			pageNo = Integer.parseInt(currentPage);
-		}
-		
-		/* 전체 게시물 수 */
 		ResellListService resellListService = new ResellListService();
 
 		List<BoardDTO> resellList = resellListService.selectResellList();
 		
-		
-		
 		SelectCriteria selectCriteria = null;
 		
-		selectCriteria = Pagenation.getSelectCriteria(pageNo, resellList.size(), limit, buttonAmount);
-		
-		/* 게시판 정보 조회 */
-		System.out.println(selectCriteria);
+		selectCriteria = Pagenation.getSelectCriteria(pageNo, 5, limit, buttonAmount);
 		
 		String path = "";
 		if (resellList != null) {
