b75e0c66004c77e042c92b169cafdd719a6189f4
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2021-10-18 18:15:52 +0900

    20211018 18:15 황성연
    -

---
+++ src/com/theshoes/jsp/board/contorller/CommentsDeleteServlet.java
@@ -0,0 +1,33 @@
+package com.theshoes.jsp.board.contorller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.theshoes.jsp.board.model.service.ResellListService;
+
+@WebServlet("/resell/comments/delete")
+public class CommentsDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int commentNo = (Integer.valueOf(request.getParameter("deleteComment")));
+		int boardNo = Integer.parseInt(request.getParameter("no"));
+		
+		ResellListService resellListService = new ResellListService();
+		
+		int result = resellListService.deleteComment(commentNo);
+		
+		if(result > 0) {
+			request.setAttribute("successCode", "deleteComments");
+			request.setAttribute("no", boardNo + "");
+			request.getRequestDispatcher("/WEB-INF/views/common/success.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/WEB-INF/views/common/errorPage.jsp").forward(request, response);
+		}
+	}
+
+}
